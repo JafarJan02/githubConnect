18351cd598db4dc6e5ff389d1e65f0908204d8db
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-18 07:50:26 +0300

    Initial Commit

---
+++ githubConnect/githubConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubConnectApp.swift
+//  githubConnect
+//
+//  Created by Jafar on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct githubConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
